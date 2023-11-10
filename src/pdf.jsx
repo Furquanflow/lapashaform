8b9e47a5185ed8281b3a0e04c6c1a74ff4cef18b
Furquanflow <furqan.rahim@flowtechnologies.io>
2023-11-11 03:17:32 +0500

    Remove /

--- src/pdf.jsx
+++ src/pdf.jsx
@@ -9,10 +9,11 @@ const GeneratePDFButton = ({ formData }) => {
         try {
             const formDataToSend = new FormData();
             formDataToSend.append('data', JSON.stringify(formData));
-            const response = await axios.post(`/generate-and-send-pdf`, formDataToSend);
+            const response = await axios.post(`generate-and-send-pdf`, formDataToSend);
             if (response.status === 200) {
                 alert('PDF generated and sent successfully.');
                 window.open(`${baseUrl}/download-pdf`, '_blank');
+                console.log("Workign");
             } else {
                 alert('Failed to generate and send PDF.');
             }

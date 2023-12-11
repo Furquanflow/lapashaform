d3558dab5b6e151cfa5e60af6ca1f1ee12b4f1e1
Furquanflow <furqan.rahim@flowtechnologies.io>
2023-12-12 02:22:26 +0500

    Add email code

--- src/pdf.jsx
+++ src/pdf.jsx
@@ -18,20 +18,20 @@ const GeneratePDFButton = ({ formData, pdfCount }) => {
     try {
       const formDataToSend = new FormData();
       formDataToSend.append("data", JSON.stringify(formData));
-      // const response = await axios.post(
-      //   `${baseUrl}/${pdfCount >= 1
-      //     ? "generate-and-send-pdf"
-      //     : "generate-and-send-pdf-employer"}`,
-      //   formDataToSend
-      // );
-      // if (response.data && response.data.pdfPath) {
+      const response = await axios.post(
+        `${baseUrl}/${pdfCount >= 1
+          ? "generate-and-send-pdf"
+          : "generate-and-send-pdf-employer"}`,
+        formDataToSend
+      );
+      if (response.data && response.data.pdfPath) {
         alert("PDF generated and sent successfully.");
         window.open(`${baseUrl}/download-pdf`, "_blank");
         console.log("Condition Working");
         navigate("/stepform");
-      // } else {
-      //   alert("Failed to generate and send PDF.");
-      // }
+      } else {
+        alert("Failed to generate and send PDF.");
+      }
     } catch (error) {
       console.error("Error:", error);
     }
